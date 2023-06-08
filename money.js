09bb61857feda80d2db5f9dd824698417aebc6ee
Bohuslav66 <49689844+Bohuslav66@users.noreply.github.com>
2023-06-08 13:06:15 +0200

    Add files via upload

--- money.js
+++ money.js
@@ -13,11 +13,69 @@ async function initMap() {
     mapId: "map",
   });
 
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Marker_1",
+  google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng);
+  });
+ 
+  function placeMarker(location) {
+    var marker = new google.maps.Marker({
+      position: location, 
+      map: map
+    });
+  }
+
+  
+
+  marker.addListener("click", ({ domEvent, latLng }) => {
+    const { target } = domEvent;
+
+    infoWindow.close();
+    infoWindow.setContent(marker.title);
+    infoWindow.open(marker.map, marker);
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
   });
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
 
 initMap();
